3efe5365712912cbdd55389365c89d318c095947
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-11-01 22:27:46 +0330

    complate order app + veiws + forms

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
+
+app_name = 'order'
 
 urlpatterns = [
-    
+    path('create/', views.OrderCreate.as_view(), name='order_create'),    
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,39 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+from .models import OrderItem
+from .forms import OrderCerateForm
+from cart.cart import Cart
+
+
+class OrderCreate(View):
+    '''
+    This class is for create order products.
+    '''
+
+    def get(self, request):
+        form = OrderCerateForm()
+        cart = Cart(request)
+        
+        context = {'form': form, 'cart': cart}
+        return render(request, 'order/create.html', context)
+
+    def post(self, request):
+        cart = Cart(request)
+        form = OrderCerateForm(request.POST)
+
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+
+            # clear the cart
+            cart.clear()
+        
+        context = {'order': order}
+        return render(request, 'order/create.html', context)
